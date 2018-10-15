23eae9cab6ac826286fdd4fc15a1bf31b91b0868
danielferreiradf <danielferreiradk@gmail.com>
2018-10-15 11:00:45 -0300

    setup heroku

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Server listening on ${port}`);

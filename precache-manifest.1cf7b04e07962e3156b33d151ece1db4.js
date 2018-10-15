0a338c0837100e47ab2abc8445f3e3837ccf8210
danielferreiradf <danielferreiradk@gmail.com>
2018-10-14 22:40:33 -0300

    Updates

---
+++ precache-manifest.1cf7b04e07962e3156b33d151ece1db4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1b851ce5494db3538e591a88369282",
+    "url": "/monstermovies/static/media/bgseries.4d1b851c.jpg"
+  },
+  {
+    "revision": "82200183d9abe5d72c00",
+    "url": "/monstermovies/static/css/main.912b3ced.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/monstermovies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a8fe80dba66df28c675d",
+    "url": "/monstermovies/static/js/1.a8fe80db.chunk.js"
+  },
+  {
+    "revision": "dc36aedd295c6f0b19a3",
+    "url": "/monstermovies/static/js/runtime~main.dc36aedd.js"
+  },
+  {
+    "revision": "9504a2dc73499ab4fcb3a898bad0a551",
+    "url": "/monstermovies/static/media/logo.9504a2dc.png"
+  },
+  {
+    "revision": "ac4471291c620d8dd47697a1d8da4975",
+    "url": "/monstermovies/static/media/spinner.ac447129.gif"
+  },
+  {
+    "revision": "82200183d9abe5d72c00",
+    "url": "/monstermovies/static/js/main.82200183.chunk.js"
+  },
+  {
+    "revision": "339048d6c7c594ba88e16c2e6287c86d",
+    "url": "/monstermovies/static/media/bgmovies.339048d6.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/monstermovies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/monstermovies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/monstermovies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/monstermovies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a8fe80dba66df28c675d",
+    "url": "/monstermovies/static/css/1.5f75551d.chunk.css"
+  },
+  {
+    "revision": "24e99c7c992099e5df2aea447b408c0c",
+    "url": "/monstermovies/index.html"
+  }
+];
